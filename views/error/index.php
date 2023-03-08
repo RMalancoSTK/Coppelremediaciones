cf2c29706765f6cf1a0936dcb40db52426c0f321
Rafael Malanco <rafael.malanco@softtek.com>
2023-03-08 10:50:15 -0600

    update

--- views/error/index.php
+++ views/error/index.php
@@ -1,7 +1,9 @@
 <div class="row justify-content-center">
     <div class="col-lg-6">
         <div class="text-center mt-4">
-            <img class="mb-4 img-error" src="assets/img/error-404-monochrome.svg" />
+            <h1 class="display-1 text-danger">
+                <i class="fas fa-exclamation-triangle"></i>
+            </h1>
             <p class="lead">Esta página no está disponible</p>
             <a href="<?= BASE_URL ?>">
                 <i class="fas fa-arrow-left me-1"></i>
